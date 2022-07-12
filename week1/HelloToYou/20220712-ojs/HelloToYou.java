a0846812a304d0590ed64e5ea80de1ee0f894639
OHJINSEONG <ojseong0828@naver.com>
2022-07-12 15:54:56 +0900

    20220712-HelloToYou-오진성
    두번째 반복과제 복구 성공..

--- week1/HelloToYou/20220712-ojs/HelloToYou.java
+++ week1/HelloToYou/20220712-ojs/HelloToYou.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HelloToYou {
+import java.util.Scanner;
+
+public class HelloToYou {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //이름이 뭐니?
+        System.out.print("What's your name? ");
+
+        //이름
+        String answer = scanner.nextLine();
+
+        // 안녕, 이름!
+        System.out.print("Hello, " + answer + "!");
+    }
 }
