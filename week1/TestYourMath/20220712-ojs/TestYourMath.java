6940c97aed1b3e6e0508c76bf28030ecba69f2b8
OHJINSEONG <ojseong0828@naver.com>
2022-07-12 16:12:25 +0900

    20220712-TestYourMath-오진성
    세번째 무한 반복과제 완료!

--- week1/TestYourMath/20220712-ojs/TestYourMath.java
+++ week1/TestYourMath/20220712-ojs/TestYourMath.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class TestYourMath {
+import java.util.Scanner;
+
+public class TestYourMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //숫자 두개를 기입하여라!
+        System.out.print("Input 2 numbers: ");
+
+        //기입
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+
+        //덧셈 문제
+        System.out.println(input1 + " + " + input2 + " = ?");
+
+        //덧셈 답
+        int answer1 = scanner.nextInt();
+
+        //곱셈 문제
+        System.out.println(input1 + " * " + input2 + " = ?");
+
+        //곱셈 답
+        int answer2 = scanner.nextInt();
+
+        boolean results1 = answer1 == (input1 + input2);
+        boolean results2 = answer2 == (input1 * input2);
+
+        //지능 판다
+        if (results1 && results2) {
+            System.out.println("Genius!");
+        }
+        if (!results1 && !results2) {
+            System.out.println("Stupid!");
+        }
+        if ((!results1 && results2) || (results1 && !results2)) {
+            System.out.println("Muggle!");
+
+        }
+    }
 }
