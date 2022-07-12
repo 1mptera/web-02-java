9f425465080f0bc7c9669236b1cd4ee99ab2fbce
jingwook <qnfdkf153@gmail.com>
2022-07-12 20:55:21 +0900

    20220712-RockScissorsPaper-오진욱
    마무으리!

---
+++ week01/RockScissorsPaper/20220712-오진욱/RockScissorsPaper.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockScissorsPaper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        double randomNumber = random.nextDouble(3);
+
+        String computerPick = null;
+
+        System.out.println("가위바위보!");
+        System.out.print("나: " );
+        String yourPick = scanner.nextLine();
+
+        if(randomNumber <=1){
+            computerPick = "가위";
+        }
+        if(randomNumber >=1 && randomNumber <=2  ){
+            computerPick = "바위";
+        }
+        if(randomNumber >=2 && randomNumber <=3 ){
+            computerPick = "보";
+        }
+
+        System.out.println("상대: " + computerPick);
+
+        boolean comRock = computerPick.equals("바위");
+        boolean comScissor = computerPick.equals("가위");
+        boolean comPaper = computerPick.equals("보");
+
+        boolean yourRock = computerPick.equals("바위");
+        boolean yourScissor = computerPick.equals("가위");
+        boolean yourPaper = computerPick.equals("보");
+
+        if (computerPick.equals(yourPick)){
+            System.out.println("비겼다!");
+        }
+
+        if((yourRock && comScissor) || (yourScissor && comPaper) || (yourPaper && comRock) ){
+            System.out.println("이겼다!");
+        }
+
+        if((yourRock && comPaper) || (yourScissor && comRock) || (yourPaper && comScissor) ){
+            System.out.println("졌다!");
+        }
+    }
+}
