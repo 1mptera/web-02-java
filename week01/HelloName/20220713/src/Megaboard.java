ded005ddd28f71650c903f718a71c18671e37233
junhyeong00 <jhbae0420@naver.com>
2022-07-13 09:48:56 +0900

    20220713-HelloName-배준형
    구현완료

---
+++ week01/HelloName/20220713/src/Megaboard.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Megaboard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("이용시간(분): ");
+
+        int time = scanner.nextInt();
+
+        System.out.print("파킹존 주차여부: ");
+
+        boolean parking = scanner.nextBoolean();
+
+        int fee;
+
+        if (parking == true) {
+
+        }
+
+        }
+
+
+
+        System.out.println("요금: " + fee);
+
+    }
