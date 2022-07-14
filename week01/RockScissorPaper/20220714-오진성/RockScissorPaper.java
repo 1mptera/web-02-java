4ac8dbd29ee78987abf1887b991d11865b00b55a
OHJINSEONG <ojseong0828@naver.com>
2022-07-14 20:05:41 +0900

    20220714-RockScissorPaper-오진성
    피드백 수정: 의미없는 변수 삭제

---
+++ week01/RockScissorPaper/20220714-오진성/RockScissorPaper.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockScissorPaper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int num = random.nextInt(3);
+        //가위바위보!
+        System.out.print("가위바위보!\n나: ");
+
+        //이얍!
+        String answer = scanner.nextLine();
+
+        //상대방
+        if (num == 0){
+            System.out.println("상대편: 바위");
+            if(answer.equals("바위")){
+                System.out.println("비겼다!");
+            }
+            if(answer.equals("가위")){
+                System.out.println("졌다!");
+            }
+            if(answer.equals("보")){
+                System.out.println("이겼다!");
+            }
+        }
+        if (num == 1){
+            System.out.println("상대편: 가위");
+            if(answer.equals("바위")){
+                System.out.println("이겼다!");
+            }
+            if(answer.equals("가위")){
+                System.out.println("비겼다!");
+            }
+            if(answer.equals("보")){
+                System.out.println("졌다!");
+            }
+        }
+        if (num == 2){
+            System.out.println("상대편: 보");
+            if(answer.equals("바위")){
+                System.out.println("졌다!");
+            }
+            if(answer.equals("가위")){
+                System.out.println("이겼다!");
+            }
+            if(answer.equals("보")){
+                System.out.println("비겼다!");
+            }
+        }
+    }
+}
