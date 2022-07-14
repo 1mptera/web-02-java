05a73b245058b23d5793be8f622bfff6ee636d27
jingwook <qnfdkf153@gmail.com>
2022-07-14 09:03:17 +0900

    빈줄 제거 및 축약형 제거

--- week01/RandomDice/20220712-오진욱/RandomDice.java
+++ week01/RandomDice/20220712-오진욱/RandomDice.java
@@ -3,25 +3,21 @@ import java.util.Scanner;
 
 public class RandomDice {
     public static void main(String[] args) {
-
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-
-
-        int diceNum = random.nextInt(6) + 1;
+        int diceNumber = random.nextInt(6) + 1;
 
         System.out.println("주사위를 맞춰라!");
         System.out.print("예측 수 : ");
 
-        int predictNum = scanner.nextInt();
+        int predictNumber = scanner.nextInt();
 
-        System.out.println("주사위 수 : " + diceNum);
+        System.out.println("주사위 수 : " + diceNumber);
 
-        if(diceNum == predictNum){
+        if(diceNumber == predictNumber){
             System.out.println("예측에 성공했습니다!");
             return;
         }
         System.out.println("예측에 실패했습니다!");
-
     }
 }
