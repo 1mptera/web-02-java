41d13f224a187eed30210c8e4786ddbd761a7b6e
jingwook <qnfdkf153@gmail.com>
2022-07-15 20:41:32 +0900

    맨 끝줄 개행문자 추가 및 불필요한 빈 줄 제거

--- week01/Megaboard/오진욱/Megaboard.java
+++ week01/Megaboard/오진욱/Megaboard.java
@@ -4,13 +4,11 @@ public class Megaboard {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-
         System.out.print("이용시간: ");
         int usedMin = scanner.nextInt();
 
         System.out.print("파킹존 주차여부: ");
         boolean isParkingzone = scanner.nextBoolean();
-
 
         double totalFee = 1000 + 150 * usedMin;
 
@@ -23,6 +21,5 @@ public class Megaboard {
         }
 
         System.out.println("요금: " + totalFee + "원");
-
     }
 }
