907f54b8a42d95abc04be7c647d983808ea23d41
junhyeong00 <jhbae0420@naver.com>
2022-07-16 10:50:17 +0900

    하드코딩이 아닌 형태로 변경
    문제 해결

--- week01/LaunchWorldCup/배준형/src/LaunchWorldCup.java
+++ week01/LaunchWorldCup/배준형/src/LaunchWorldCup.java
@@ -9,8 +9,13 @@ public class LaunchWorldCup {
         System.out.println("1. 중식\n2. 일식\n3. 한식\n4. 양식");
 
         int option = scanner.nextInt();
+
+        String food1 = "";
+        String food2 = "";
+
         //중식
         if (option == 1) {
+
             System.out.println("1. 짜장 vs 2. 짬뽕");
 
             int option1 = scanner.nextInt();
@@ -20,42 +25,24 @@ public class LaunchWorldCup {
             int option2 = scanner.nextInt();
 
             //최종선택
-            if (option1 == 1 && option2 == 1) {
+            if (option1 == 1) {
 
-                System.out.println("1. 짜장 vs 2. 볶음밥");
+                food1 = "짜장";
             }
 
-            if (option1 == 1 && option2 == 2) {
+            if (option1 == 2) {
 
-                System.out.println("1. 짜장 vs 2. 잡채밥");
+                food1 = "짬뽕";
             }
 
-            if (option1 == 2 && option2 == 1) {
+            if (option2 == 1) {
 
-                System.out.println("1. 짬뽕 vs 2. 볶음밥");
+                food2 = "볶음밥";
             }
 
-            if (option1 == 2 && option2 == 2) {
-                System.out.println("1. 짬뽕 vs 2. 잡채밥");
-            }
+            if (option2 == 2) {
 
-            int result = scanner.nextInt();
-
-            //중식 결과
-            if (option1 == 1 && result == 1) {
-                System.out.println("오늘의 점심은 짜장입니다!");
-            }
-
-            if (option1 == 2 && result == 1) {
-                System.out.println("오늘의 점심은 짬뽕입니다!");
-            }
-
-            if (option2 == 1 && result == 2) {
-                System.out.println("오늘의 점심은 볶음밥입니다!");
-            }
-
-            if (option2 == 2 && result == 2) {
-                System.out.println("오늘의 점심은 잡채밥입니다!");
+                food2 = "잡채밥";
             }
         }
 
@@ -70,44 +57,27 @@ public class LaunchWorldCup {
             int option2 = scanner.nextInt();
 
             //최종선택
-            if (option1 == 1 && option2 == 1) {
+            if (option1 == 1) {
 
-                System.out.println("1. 스시 vs 2. 우동");
+                food1 = "스시";
             }
 
-            if (option1 == 1 && option2 == 2) {
+            if (option1 == 2) {
 
-                System.out.println("1. 스시 vs 2. 돈까스");
+                food1 = "덴뿌라";
             }
 
-            if (option1 == 2 && option2 == 1) {
+            if (option2 == 1) {
 
-                System.out.println("1. 덴뿌라 vs 2. 우동");
+                food2 = "우동";
             }
 
-            if (option1 == 2 && option2 == 2) {
-                System.out.println("1. 덴뿌라 vs 2. 돈까스");
-            }
+            if (option2 == 2) {
 
-            int result = scanner.nextInt();
-
-            //일식 결과
-            if (option1 == 1 && result == 1) {
-                System.out.println("오늘의 점심은 스시입니다!");
-            }
-
-            if (option1 == 2 && result == 1) {
-                System.out.println("오늘의 점심은 덴뿌라입니다!");
-            }
-
-            if (option2 == 1 && result == 2) {
-                System.out.println("오늘의 점심은 우동입니다!");
-            }
-
-            if (option2 == 2 && result == 2) {
-                System.out.println("오늘의 점심은 돈까스입니다!");
+                food2 = "돈까스";
             }
         }
+
         //양식
         if (option == 4) {
             System.out.println("1. 파스타 vs 2. 리조또");
@@ -119,44 +89,40 @@ public class LaunchWorldCup {
             int option2 = scanner.nextInt();
 
             //최종선택
-            if (option1 == 1 && option2 == 1) {
+            if (option1 == 1) {
 
-                System.out.println("1. 파스타 vs 2. 스테이크");
+                food1 = "파스타";
             }
 
-            if (option1 == 1 && option2 == 2) {
+            if (option1 == 2) {
 
-                System.out.println("1. 파스타 vs 2. 피자");
+                food1 = "리조또";
             }
 
-            if (option1 == 2 && option2 == 1) {
+            if (option2 == 1) {
 
-                System.out.println("1. 리조또 vs 2. 스테이크");
+                food2 = "스테이크";
             }
 
-            if (option1 == 2 && option2 == 2) {
-                System.out.println("1. 리조또 vs 2. 피자");
-            }
+            if (option2 == 2) {
 
-            int result = scanner.nextInt();
-
-            //양식 결과
-            if (option1 == 1 && result == 1) {
-                System.out.println("오늘의 점심은 파스타입니다!");
-            }
-
-            if (option1 == 2 && result == 1) {
-                System.out.println("오늘의 점심은 리조또입니다!");
-            }
-
-            if (option2 == 1 && result == 2) {
-                System.out.println("오늘의 점심은 스테이크입니다!");
-            }
-
-            if (option2 == 2 && result == 2) {
-                System.out.println("오늘의 점심은 피자입니다!");
+                food2 = "피자";
             }
         }
+
+        System.out.println("1. " + food1 + " vs 2. " + food2);
+
+        int result = scanner.nextInt();
+
+        //결과
+        if (result == 1) {
+            System.out.println("오늘의 점심은 " + food1 + "입니다!");
+        }
+
+        if (result == 2) {
+            System.out.println("오늘의 점심은 " + food2 + "입니다!");
+        }
+
         //한식
         if (option == 3) {
             System.out.println("한식은 선택지가 없습니다.\n 오늘의 점심은 제욱볶음입니다.");
