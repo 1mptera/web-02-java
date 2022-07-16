fe03c42084fc97d34273a8b1e574b7d069240e86
OHJINSEONG <ojseong0828@naver.com>
2022-07-16 19:32:43 +0900

    퀘스트 피드백 수정
    완료!

--- week01/LaunchWorldCup/20220714-오진성/LaunchWorldCup.java
+++ week01/LaunchWorldCup/20220714-오진성/LaunchWorldCup.java
@@ -6,120 +6,147 @@ public class LaunchWorldCup {
 
         //점심 월드컵 시즈악!
         System.out.println("점심 월드컵!\n-------카테고리 선택-------");
-        System.out.println("1. 중식\n2. 일식\n3. 한식\n4. 양식");
+        System.out.println("1. 중식");
+        System.out.println("2. 일식");
+        System.out.println("3. 한식");
+        System.out.println("4. 양식");
 
-        //너의 선택은?
+        String chiniesemenu1 = "짜장";
+        String chiniesemenu2 = "짬뽕";
+        String chiniesemenu3 = "볶음밥";
+        String chiniesemenu4 = "잡채밥";
+
         int answer1 = scanner.nextInt();
 
-        //중식
         if (answer1 == 1) {
-            System.out.println("1.짜장 vs 2.짬뽕");
+            System.out.println("1." + chiniesemenu1 + " vs 2." + chiniesemenu2);
+
             int answer2 = scanner.nextInt();
-            System.out.println("1.볶음밥 vs 2.잡채밥");
+
+            System.out.println("1. " + chiniesemenu3 + " vs 2." + chiniesemenu4);
+
             int answer3 = scanner.nextInt();
+
             if (answer2 == 1 && answer3 == 1) {
-                System.out.println("1.짜장 vs 2.볶음밥");
+                System.out.println("1." + chiniesemenu1 + " vs 2." + chiniesemenu3);
             }
             if (answer2 == 2 && answer3 == 1) {
-                System.out.println("1.짬뽕 vs 2.볶음밥");
+                System.out.println("1." + chiniesemenu2 + " vs 2." + chiniesemenu3);
             }
             if (answer2 == 1 && answer3 == 2) {
-                System.out.println("1.짜장 vs 2.잡채밥");
+                System.out.println("1." + chiniesemenu1 + " vs 2." + chiniesemenu4);
             }
             if (answer2 == 2 && answer3 == 2) {
-                System.out.println("1.짬뽕 vs 2.잡채밥");
+                System.out.println("1." + chiniesemenu2 + " vs 2." + chiniesemenu4);
             }
+
             int answer4 = scanner.nextInt();
 
-            //점심결정!!
             if (answer2 == 1 && answer4 == 1) {
-                System.out.println("오늘의 점심은 짜장입니다!");
+                System.out.println("오늘의 점심은 " + chiniesemenu1 + "입니다!");
             }
             if (answer2 == 2 && answer4 == 1) {
-                System.out.println("오늘의 점심은 짬뽕입니다!");
+                System.out.println("오늘의 점심은 " + chiniesemenu2 + "입니다!");
             }
             if (answer3 == 1 && answer4 == 2) {
-                System.out.println("오늘의 점심은 볶음밥입니다!");
+                System.out.println("오늘의 점심은 " + chiniesemenu3 + "입니다!");
             }
             if (answer3 == 2 && answer4 == 2) {
-                System.out.println("오늘의 점심은 잡채밥입니다!");
+                System.out.println("오늘의 점심은 " + chiniesemenu4 + "입니다!");
             }
         }
 
-        //일식
+        String japanesemenu1 = "스시";
+        String japanesemenu2 = "덴뿌라";
+        String japanesemenu3 = "우동";
+        String japanesemenu4 = "돈까스";
+
         if (answer1 == 2) {
-            System.out.println("1.스시 vs 2.덴뿌라");
+            System.out.println("1." + japanesemenu1 + " vs 2." + japanesemenu2);
+
             int answer2 = scanner.nextInt();
-            System.out.println("1.우동 vs 2.돈까스");
+
+            System.out.println("1." + japanesemenu3 + " vs 2." + japanesemenu4);
+
             int answer3 = scanner.nextInt();
+
             if (answer2 == 1 && answer3 == 1) {
-                System.out.println("1.스시 vs 2.우동");
+                System.out.println("1." + japanesemenu1 + " vs 2." + japanesemenu3);
             }
             if (answer2 == 2 && answer3 == 1) {
-                System.out.println("1.덴뿌라 vs 2.우동");
+                System.out.println("1." + japanesemenu2 + " vs 2." + japanesemenu3);
             }
             if (answer2 == 1 && answer3 == 2) {
-                System.out.println("1.스시 vs 2.돈까스");
+                System.out.println("1." + japanesemenu1 + " vs 2." + japanesemenu4);
             }
             if (answer2 == 2 && answer3 == 2) {
-                System.out.println("1.덴뿌라 vs 2.돈까스");
+                System.out.println("1." + japanesemenu2 + "vs 2." + japanesemenu4);
             }
+
             int answer4 = scanner.nextInt();
 
-            //점심 결정!!
             if (answer2 == 1 && answer4 == 1) {
-                System.out.println("오늘의 점심은 스시입니다!");
+                System.out.println("오늘의 점심은 " + japanesemenu1 + "입니다!");
             }
             if (answer2 == 2 && answer4 == 1) {
-                System.out.println("오늘의 점심은 덴뿌라입니다!");
+                System.out.println("오늘의 점심은 " + japanesemenu2 + "입니다!");
             }
             if (answer3 == 1 && answer4 == 2) {
-                System.out.println("오늘의 점심은 우동입니다!");
+                System.out.println("오늘의 점심은 " + japanesemenu3 + "입니다!");
             }
             if (answer3 == 2 && answer4 == 2) {
-                System.out.println("오늘의 점심은 돈까스입니다!");
+                System.out.println("오늘의 점심은 " + japanesemenu4 + "입니다!");
             }
         }
 
-        //한식
+        String koreanmenu1 = "제육볶음";
+
         if (answer1 == 3) {
 
-            //한식은 제육볶음!!
-            System.out.println("한식은 선택지가 없습니다!\n오늘의 점심은 제육볶음입니다!");
+            System.out.println("한식은 선택지가 없습니다!");
+            System.out.println("오늘의 점심은 " + koreanmenu1 + "입니다!");
         }
 
-        //양식
+        String westfoodmenu1 = "파스타";
+        String westfoodmenu2 = "리조또";
+        String westfoodmenu3 = "스테이크";
+        String westfoodmenu4 = "피자";
+
         if (answer1 == 4) {
-            System.out.println("1.파스타 vs 2.리조또");
+            System.out.println("1." + westfoodmenu1 + " vs 2." + westfoodmenu2 + "");
+
             int answer2 = scanner.nextInt();
-            System.out.println("1.스테이크 vs 2.피자");
+
+            System.out.println("1." + westfoodmenu3 + " vs 2.피자");
+
             int answer3 = scanner.nextInt();
+
             if (answer2 == 1 && answer3 == 1) {
-                System.out.println("1.파스타 vs 2.스테이크");
+                System.out.println("1." + westfoodmenu1 + " vs 2." + westfoodmenu3);
             }
             if (answer2 == 2 && answer3 == 1) {
-                System.out.println("1.리조또 vs 2.스테이크");
+                System.out.println("1." + westfoodmenu2 + " vs 2." + westfoodmenu3);
             }
             if (answer2 == 1 && answer3 == 2) {
-                System.out.println("1.파스타 vs 2.피자");
+                System.out.println("1." + westfoodmenu1 + " vs 2." + westfoodmenu4);
             }
             if (answer2 == 2 && answer3 == 2) {
-                System.out.println("1.리조또 vs 2.피자");
+                System.out.println("1." + westfoodmenu2 + " vs 2." + westfoodmenu4);
             }
+
             int answer4 = scanner.nextInt();
 
-            //점심 결정!!
             if (answer2 == 1 && answer4 == 1) {
-                System.out.println("오늘의 점심은 파스타입니다!");
+                System.out.println("오늘의 점심은 " + westfoodmenu1 + "입니다!");
             }
             if (answer2 == 2 && answer4 == 1) {
-                System.out.println("오늘의 점심은 리조또입니다!");
+                System.out.println("오늘의 점심은 " + westfoodmenu2 + "입니다!");
             }
             if (answer3 == 1 && answer4 == 2) {
-                System.out.println("오늘의 점심은 스테이크입니다!");
+                System.out.println("오늘의 점심은 " + westfoodmenu3 + "입니다!");
             }
             if (answer3 == 2 && answer4 == 2) {
-                System.out.println("오늘의 점심은 피자입니다!");
+                System.out.println("오늘의 점심은 " + westfoodmenu4 + "입니다!");
             }
         }
     }
