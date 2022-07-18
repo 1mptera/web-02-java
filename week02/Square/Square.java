1456003888d72b51af996ff3e321f225090aa94f
OHJINSEONG <ojseong0828@naver.com>
2022-07-18 16:44:40 +0900

    Square-오진성
    완료

---
+++ week02/Square/Square.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String countstar = "";
+
+        while (true) {
+            System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
+
+            int answer = scanner.nextInt();
+
+            if ((answer < 2) || (answer >= 20)) {
+                System.out.println("다시 입력해주세요.");
+            }
+
+            if (answer >= 2 && answer < 20) {
+                for (int i = 0; i < answer; i += 1) {
+                    countstar += "\n";
+                    for (int j = 0; j < answer; j += 1) {
+                        countstar += "*";
+                    }
+                }
+                break;
+            }
+        }
+
+        System.out.println(countstar);
+    }
+}
