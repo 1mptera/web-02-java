49928cc7fe9558bc6ec4aad04adcabf594c32634
OHJINSEONG <ojseong0828@naver.com>
2022-07-19 07:35:20 +0900

    Triange-오진성
    피드백 수정: 변수 입름바꾸기, 불필요한 변수 제거

--- week02/Triangle/Triangle.java
+++ week02/Triangle/Triangle.java
@@ -3,31 +3,32 @@ import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String tap = "";
-        String countstar = "";
 
-        while (true) {
+        String starTriangle = "";
+
+        System.out.println("삼각형의 높이를 입력해 주세요(2 이상 20 미만)");
+
+        int height = scanner.nextInt();
+        int base = 2 * height - 1;
+
+        while ((height < 2) || (height >= 20)) {
             System.out.println("삼각형의 높이를 입력해 주세요(2 이상 20 미만)");
-            int height = scanner.nextInt();
-            int base = 2 * height - 1;
-            if ((height < 2) || (height >= 20)) {
-                System.out.println("다시 입력해 주세요.");
-                continue;
-            }
-
-            for (int i = 1; i <= height; i += 1) {
-                for (int j = 1; j <= base; j += 1) {
-                    if (j >= height - i + 1 && j <= height + i - 1) {
-                        countstar += "*";
-                        continue;
-                    }
-                    countstar += " ";
-                }
-                countstar += "\n";
-            }
-
-            System.out.println(countstar);
+            height = scanner.nextInt();
+            System.out.println("다시 입력해 주세요.");
         }
 
+        for (int i = 1; i <= height; i += 1) {
+            for (int j = 1; j <= base; j += 1) {
+                if (j >= height - i + 1 && j <= height + i - 1) {
+                    starTriangle += "*";
+                    continue;
+                }
+                starTriangle += " ";
+            }
+            starTriangle += "\n";
+        }
+
+        System.out.println(starTriangle);
     }
 }
+
