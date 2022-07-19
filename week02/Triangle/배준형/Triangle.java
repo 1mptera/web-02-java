4a31fbaae46c459815ca4b644c79c34e7403fcf5
junhyeong00 <jhbae0420@naver.com>
2022-07-19 16:24:56 +0900

    for문 삭제 및 if문 사용
    for 내부에 for가 2개 있어 복잡했다. 그래서 if문으로 대체하여 해결했다.

--- week02/Triangle/배준형/Triangle.java
+++ week02/Triangle/배준형/Triangle.java
@@ -7,20 +7,22 @@ public class Triangle {
         System.out.println("삼각형의 높이를 입력해 주세요(2 이상 20 미만)");
 
         int high = scanner.nextInt();
+        int base = high * 2 - 1;
 
-        while (2 > high || high >= 20) {
+        while ( high < 2 || high >= 20) {
             System.out.println("다시 입력해주세요");
 
             high = scanner.nextInt();
         }
 
-        for (int i = 0; i < high; i += 1) {
-            for (int j = high - 1; j > i; j -= 1) {
-                System.out.print(" ");
-            }
+        for (int i = 1; i <= high; i += 1) {
+            for (int j = 1; j <= base; j += 1) {
+                if (j >= high - i + 1  && j <= high + i - 1){
+                    System.out.print("*");
+                    continue;
+                }
 
-            for (int j = -1; j < i * 2; j += 1) {
-                System.out.print("*");
+                System.out.print(" ");
             }
 
             System.out.println();
