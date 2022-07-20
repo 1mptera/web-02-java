bfc3f22caf171139cece77bab9b9b4d32ca8c186
OHJINSEONG <ojseong0828@naver.com>
2022-07-20 16:10:57 +0900

    Circle-오진성
    피드백 수정: 코드 정리

--- week02/Circle/Circle.java
+++ week02/Circle/Circle.java
@@ -5,17 +5,17 @@ public class Circle {
         Scanner scanner = new Scanner(System.in);
 
         int Radius = scanner.nextInt();
-        int Diameter = 2*Radius +1;
+        int Diameter = 2 * Radius + 1;
 
         for (int i = 0; i < Diameter; i += 1) {
             for (int j = 0; j < Diameter; j += 1) {
-              if(Math.pow(j-Radius,2)<=Math.pow(Radius,2)-Math.pow(i-Radius,2)){
-              System.out.print("*");
-              continue;
-              }
-              System.out.print(" ");
+                if (Math.pow(j - Radius, 2) <= Math.pow(Radius, 2) - Math.pow(i - Radius, 2)) {
+                    System.out.print("*");
+                    continue;
+                }
+                System.out.print(" ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
