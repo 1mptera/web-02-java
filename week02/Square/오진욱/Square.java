7022cbaa25f0e455b3b48ae14a4e6cc2410da228
jingwook <qnfdkf153@gmail.com>
2022-07-20 16:56:26 +0900

    출력문 안의 ""제거

--- week02/Square/오진욱/Square.java
+++ week02/Square/오진욱/Square.java
@@ -12,21 +12,21 @@ public class Square {
             System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
             int squareLength = scanner.nextInt();
 
-            //처리
             if (squareLength < 2 || squareLength >= 20) {
                 System.out.println("다시 입력해주세요");
                 continue;
             }
 
+            //처리
             for (int i = 0; i < squareLength; i += 1) {
                 for (int j = 0; j < squareLength; j += 1) {
                     System.out.print("*");
                 }
-                System.out.println("");
+                System.out.println();
             }
 
             //출력
-            finish =1;
+            finish = 1;
         }
     }
 }
