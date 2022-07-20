a6524b4b0d2c299bf7e3803175be1186ca6951f9
OHJINSEONG <ojseong0828@naver.com>
2022-07-20 16:37:37 +0900

    SlotMachine-오진성
    피드백 수정 완료

--- week02/SlotMachine/SlotMachine.java
+++ week02/SlotMachine/SlotMachine.java
@@ -8,15 +8,22 @@ public class SlotMachine {
         Random random = new Random();
 
         int trypay = 1000;
-        int bonusgot = 0;
+        int bonusReward = 0;
         int credit = 5000;
 
-        int[] slotmachinenumber = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] slotMachineNumber = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int[] lotteryNumber = new int[3];
 
         //입력
-        while (true) {
+        while (credit < 10000) {
             System.out.println("Want to play? 1 .Yes, 2. No");
             int answer = scanner.nextInt();
+
+            if (answer == 2) {
+                System.out.println("bye bye ~");
+                break;
+            }
 
             credit -= trypay;
 
@@ -24,59 +31,46 @@ public class SlotMachine {
                 int x = random.nextInt(9);
                 int y = random.nextInt(9);
                 int z = random.nextInt(9);
-                ;
-                slotmachinenumber[0] = slotmachinenumber[x];
-                slotmachinenumber[1] = slotmachinenumber[y];
-                slotmachinenumber[2] = slotmachinenumber[z];
+                lotteryNumber[0] = slotMachineNumber[x];
+                lotteryNumber[1] = slotMachineNumber[y];
+                lotteryNumber[2] = slotMachineNumber[z];
             }
 
             System.out.print("Line: ");
 
             for (int i = 0; i < 3; i += 1) {
-                System.out.print(slotmachinenumber[i]);
+                System.out.print(lotteryNumber[i]);
                 System.out.print(" . ");
             }
-            System.out.println("");
+            System.out.println();
 
-            int basicgot = (slotmachinenumber[0] * 1000 + slotmachinenumber[1] * 1000 + slotmachinenumber[2] * 1000) / 20;
+            int basicReward = (lotteryNumber[0] * 1000 + lotteryNumber[1] * 1000 + lotteryNumber[2] * 1000) / 20;
 
             for (int i = 0; i < 3; i += 1) {
-                if (slotmachinenumber[i] == 7) {
-                    bonusgot = basicgot * 4;
+                if (lotteryNumber[i] == 7) {
+                    bonusReward = basicReward * 4;
                 }
-                if ((slotmachinenumber[i] == 4) || (slotmachinenumber[i] == 4) || (slotmachinenumber[i] == 4)) {
-                    bonusgot = basicgot / 2;
+                if ((lotteryNumber[i] == 4) || (lotteryNumber[i] == 4) || (lotteryNumber[i] == 4)) {
+                    bonusReward = basicReward / 2;
                 }
             }
 
             //처리
-            int got = basicgot + bonusgot;
-            credit += got;
+            int reward = basicReward + bonusReward;
+            credit += reward;
 
-            System.out.println("You won " + got + " !!!!!");
+            System.out.println("You won " + reward + " !!!!!");
             System.out.println("Credit: " + credit);
 
             //출력
-            if (credit >10000){
-                System.out.println("Winer!!");
-                System.out.println("bye bye ~");
-            break;
-            }
-
-            if (credit < 0){
+            if (credit < 0) {
                 System.out.println("Need credit? Go home!");
-                System.out.println("bye bye ~");
-            break;
-            }
-
-            if (answer == 1) {
-                continue;
-            }
-
-            if (answer == 2) {
                 System.out.println("bye bye ~");
                 break;
             }
         }
+
+        System.out.println("Winer!!");
+        System.out.println("bye bye ~");
     }
 }
