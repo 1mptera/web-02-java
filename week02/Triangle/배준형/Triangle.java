7a831a86cbc68fe946b7677f9db11fcf1098612a
junhyeong00 <jhbae0420@naver.com>
2022-07-20 22:11:01 +0900

    for문 하나로 삼각형 만들기
    완성!!!!

--- week02/Triangle/배준형/Triangle.java
+++ week02/Triangle/배준형/Triangle.java
@@ -7,23 +7,18 @@ public class Triangle {
         System.out.println("삼각형의 높이를 입력해 주세요(2 이상 20 미만)");
 
         int high = scanner.nextInt();
-        int base = high * 2 - 1;
 
-        while ( high < 2 || high >= 20) {
+        while (high < 2 || high >= 20) {
             System.out.println("다시 입력해주세요");
 
             high = scanner.nextInt();
         }
 
-        for (int i = 1; i <= high; i += 1) {
-            for (int j = 1; j <= base; j += 1) {
-                if (j >= high - i + 1  && j <= high + i - 1){
-                    System.out.print("*");
-                    continue;
-                }
+        for (int i = 0; i < high; i += 1) {
 
-                System.out.print(" ");
-            }
+            System.out.print(" ".repeat(high - (i + 1)));
+
+            System.out.print("*".repeat(2 * i + 1));
 
             System.out.println();
         }
