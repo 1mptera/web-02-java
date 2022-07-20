843f798a5e958bf50d09a0ca5890e8f7e9c6cf2a
OHJINSEONG <ojseong0828@naver.com>
2022-07-20 21:51:51 +0900

    DeliveryTycoon-오진성
    임시저장

---
+++ week02/DeliveryTycoon/DeliveryTycoon.java
@@ -0,0 +1,155 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DeliveryTycoon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] foodMenu = new String[]{"1. 햄버거", "2. 피자", "3. 치킨", "4. 커피", "5. 케이크", "6. 족발"};
+        int[] price = new int[]{1, 5, 3, 1, 2, 4};
+
+        for (int i = 0; i < 20; i += 1) {
+            int x = random.nextInt(6);
+            int y = random.nextInt(6);
+            String temp = foodMenu[x];
+            foodMenu[x] = foodMenu[y];
+            foodMenu[y] = temp;
+        }
+
+        int date = 0;
+        int money = 1000;
+        int hp = 1000;
+        int popularity = 0;
+
+        String[] storeName = new String[]{"노점상", "푸드 트럭", "구멍 가게", "레스토랑"};
+        String currentLevel = "";
+
+
+        if (money <= 2000 && popularity <= 20) {
+            currentLevel = storeName[0];
+        }
+        if (money <= 5000 && money > 2000 && popularity <= 50 && popularity > 20) {
+            currentLevel = storeName[1];
+        }
+        if (money <= 8000 && money > 5000 && popularity <= 80 && popularity > 500) {
+            currentLevel = storeName[2];
+        }
+        if (money > 8000 && popularity > 80) {
+            currentLevel = storeName[3];
+        }
+
+
+        System.out.println("---------------");
+        System.out.println("딜리버리 타이쿤 게임");
+        System.out.println("---------------");
+        System.out.println("메뉴를 선택해주세요");
+        System.out.println("1. 게임시작");
+        System.out.println("2. 게임종료");
+
+        int choose = scanner.nextInt();
+
+        System.out.println("주인공 이름을 입력해주세요");
+
+        String name = scanner.next();
+
+
+        while (true) {
+            date += 1;
+
+            System.out.println(date + "일차 " + name + "의 " + currentLevel);
+            System.out.println("--------------------------------------------");
+            System.out.println("체력: " + hp + ", 자산: " + money + "만원, 인지도: " + popularity);
+            System.out.println("--------------------------------------------");
+            System.out.println("1. 가게 오픈하기");
+            System.out.println("2. 상점");
+            System.out.println("3. 메뉴로 돌아가기");
+
+            choose = scanner.nextInt();
+
+
+            if (choose == 1) {
+                for (int i = 0; i < 5; i += 1) {
+
+                    System.out.println("--------------------------------------------");
+                    System.out.println("체력: " + hp + ", 자산: " + money + "만원, 인지도: " + popularity);
+                    System.out.println("--------------------------------------------");
+                    System.out.println("6개의 주문이 들어 왔습니다. (10초 안에 음식을 완성해주세요.)");
+
+                    int randomFood = random.nextInt(6);
+                    for (int j = 0; j <= randomFood; j += 1) {
+                        System.out.println(foodMenu[j]);
+                        System.out.println("음식준비:");
+                        choose = scanner.nextInt();
+                        if( )
+                    }
+
+                }
+            }
+            while (choose != 4) {
+                if (choose == 2) {
+                    System.out.println("-----------------------");
+                    System.out.println("플렉스 상점");
+                    System.out.println("-----------------------");
+                    System.out.println("1. 자양강장제: 체력 100 회복 -10만원");
+                    System.out.println("2. 광고: 인지도 +10 -300만원");
+                    System.out.println("3. 도박: 50% 확률로 자산 X 2 - 50만원");
+                    System.out.println("4. 상점 나가기");
+
+                    choose = scanner.nextInt();
+
+                    if (choose == 1) {
+                        hp += 100;
+                        money -= 10;
+                        System.out.println("자양강장제 구입! 내 자산: " + money);
+                        System.out.println("자양강장제 구입! 내 체력: " + hp);
+                        continue;
+                    }
+                    if (choose == 2) {
+                        popularity += 10;
+                        money -= 300;
+                        System.out.println("광고 구입! 내 자산: " + money);
+                        System.out.println("인지도 상승! 내 인지도: " + popularity);
+                        continue;
+                    }
+                    if (choose == 3) {
+                        int gambling = random.nextInt(2);
+                        boolean succes = gambling == 1;
+                        money -= 50;
+
+                        System.out.println("복권 구입! 내 자산: " + money);
+
+                        if (succes) {
+                            money *= 2;
+                            System.out.println("복권 성공! 내 자산: " + money);
+                        }
+                        if (!succes) {
+                            System.out.println("복권 실패! 내 자산: " + money);
+                        }
+                        continue;
+                    }
+                }
+            }
+
+
+            if (choose == 3) {
+                System.out.println("메뉴를 선택해주세요");
+                System.out.println("1. 게임시작");
+                System.out.println("2. 게임종료");
+
+                choose = scanner.nextInt();
+                if (choose == 1) {
+                    System.out.println("주인공 이름을 입력해주세요");
+
+                    name = scanner.next();
+                    continue;
+                }
+                if (choose == 2) {
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
