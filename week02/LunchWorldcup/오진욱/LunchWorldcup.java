06c49e06c0852fe293d1d72a08df5bb12b7309fe
jingwook <qnfdkf153@gmail.com>
2022-07-20 20:24:14 +0900

    배열의 배열을 사용해 간결한 코드 완성!
    기존에 더러운 코드를 싹 갈아엎고 확장성있게 배열을 만들수 있는 코드를 작성했음:

--- week02/LunchWorldcup/오진욱/LunchWorldcup.java
+++ week02/LunchWorldcup/오진욱/LunchWorldcup.java
@@ -5,101 +5,71 @@ public class LunchWorldcup {
         //준비
         Scanner scanner = new Scanner(System.in);
 
-        String[] round16 = new String[]{"피자", "햄버거", "돈까스", "카레", "짜장면", "탄탄면", "떡볶이", "파스타", "연어덮밥", "초밥",
-                "라멘", "쌀국수", "곰탕", "국밥", "파타이", "텐동", "제육볶음", "쭈꾸미 볶음", "부대찌개", "김치찌개", "우동", "냉면",
-                "냉모밀", "국수", "생선구이", "육회비빔밥", "물회국수", "뼈해장국", "닭갈비", "마라탕", "샌드위치", "타코"};
+        int roundCount = 6;
 
-        String[] round8 = new String[16];
-        String[] round4 = new String[8];
-        String[] semiFinalRound = new String[4];
-        String[] finalRound = new String[2];
-        String winner = "";
+        String[][] round = new String[roundCount - 1][];
 
-        int[] roundLength = new int[]{round16.length, round8.length, round4.length, semiFinalRound.length, finalRound.length};
+        for (int i = 1; i <= round.length; i += 1) {
+            int length = (int) Math.pow(2, round.length - i);
+            round[i - 1] = new String[length];
+        }
 
-        boolean firstOption = true;
-        boolean secondOption = true;
-        boolean isNot1or2 = false;
+        String[] foodDB = new String[]{"피자", "햄버거", "돈까스", "카레", "짜장면", "탄탄면", "떡볶이", "파스타", "연어덮밥", "초밥", "라멘", "쌀국수", "곰탕", "국밥", "파타이", "텐동", "제육볶음", "쭈꾸미 볶음", "부대찌개", "김치찌개", "우동", "냉면", "냉모밀", "국수", "생선구이", "육회비빔밥", "물회국수", "뼈해장국", "닭갈비", "마라탕", "샌드위치", "타코"};
 
-        int selectFood = 0;
+        for (int i = 0; i < round.length; i += 1) {
 
-        for (int i = 0; i < roundLength.length; i += 1) {
-            String[] round = new String[roundLength[i]];
-            String[] nextRound = new String[roundLength[i] / 2];
+            if (i == 0) {
+                for (int j = 0; j < round[i].length; j += 1) {
+                    round[i][j] = foodDB[j];
 
-            for (int j = 0; j < round.length; j += 1) {
-                if (i == 0) {
-                    round[j] = round16[j];
                 }
-                if (i == 1) {
-                    round[j] = round8[j];
-                }
-                if (i == 2) {
-                    round[j] = round4[j];
-                }
-                if (i == 3) {
-                    round[j] = semiFinalRound[j];
-                }
-                if (i == 4) {
-                    round[j] = finalRound[j];
-                }
+                continue;
             }
 
             System.out.println("");
             System.out.println("-----------------");
-            System.out.println("점심 메뉴 월드컵 " + roundLength[i] + "강");
+
+            if (i < round.length - 1) {
+                System.out.println("점심 메뉴 월드컵 " + round[i].length + "강");
+            }
+
+            if (i == round.length - 1) {
+                System.out.println("점심 메뉴 월드컵 결승전!");
+            }
+
             System.out.println("-----------------");
             System.out.println("");
             System.out.println("선호하는 메뉴를 번호로 선택해주세요");
             System.out.println("");
 
-            for (int j = 0; j < roundLength[i]; j += 2) {
-                for (int k = 0; k < 2; k += 1) {
-                    System.out.println((k + 1) + ". " + round[j + k]);
-                }
+            for (int j = 0; j < round[i - 1].length; j += 2) {
+                int x = j;
+                System.out.println("1. " + round[i - 1][x]);
 
-                selectFood = scanner.nextInt();
-                firstOption = selectFood == 1;
-                secondOption = selectFood == 2;
+                x += 1;
+                System.out.println("2. " + round[i - 1][x]);
 
-                if (!firstOption && !secondOption) {
-                    System.out.println("1번과 2번중에 입력해주세요!");
+                int select = scanner.nextInt();
 
-                    isNot1or2 = true;
-
+                if (select != 1 && select != 2) {
+                    System.out.println("다시 입력해주세요!");
                     j -= 2;
                     continue;
                 }
 
-                if (firstOption) {
-                    nextRound[j / 2] = round[j];
+                if (select == 1) {
+                    round[i][j / 2] = round[i - 1][j];
                 }
 
-                if (secondOption) {
-                    nextRound[j / 2] = round[j + 1];
-                }
-                winner = nextRound[0];
-            }
-
-            for (int j = 0; j < nextRound.length; j +=1){
-                if (i == 0){
-                    round8[j] = nextRound[j];
-                }
-                if (i == 1){
-                    round4[j] = nextRound[j];
-                }
-                if (i == 2){
-                    semiFinalRound[j] = nextRound[j];
-                }
-                if (i == 3){
-                    finalRound[j] = nextRound[j];
-                }
-                if (i ==4 ){
-                    winner = nextRound[j];
+                if (select == 2) {
+                    round[i][j / 2] = round[i - 1][j + 1];
                 }
             }
         }
 
-        System.out.println("오늘의 점심은 " + winner + " 입니다.");
+        String lunch = round[round.length - 1][0];
+
+        System.out.println();
+        System.out.println("오늘의 점심은 " + lunch + " 입니다");
     }
 }
