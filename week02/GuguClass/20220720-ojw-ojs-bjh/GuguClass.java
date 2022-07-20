7107256eca59aaa5365ab7ed98a594167530d39d
jingwook <qnfdkf153@gmail.com>
2022-07-20 14:33:33 +0900

    GuguClass 3차 커밋

--- week02/GuguClass/20220720-ojw-ojs-bjh/GuguClass.java
+++ week02/GuguClass/20220720-ojw-ojs-bjh/GuguClass.java
@@ -8,20 +8,26 @@ public class GuguClass {
     public static void main(String[] args) {
         for (int i = 2; i < 10; i += 3) {
             for (int j = 1; j < 10; j += 1) {
-                System.out.print(i + (" * ") + j + " = " + i * j);
+                int x = i;
+
+                System.out.print(x + (" * ") + j + " = " + x * j);
 
                 System.out.print("\t");
 
-                System.out.print((i + 1) + (" * ") + j + " = " + (i + 1) * j);
+                x += 1;
+
+                System.out.print(x + (" * ") + j + " = " + x * j);
 
                 System.out.print("\t");
 
-                if (i + 2 == 10) {
+                x += 1;
+
+                if (x == 10) {
                     System.out.println();
                     continue;
                 }
 
-                System.out.print((i + 2) + (" * ") + j + " = " + (i + 2) * j);
+                System.out.print(x + (" * ") + j + " = " + x * j);
 
                 System.out.println();
             }
