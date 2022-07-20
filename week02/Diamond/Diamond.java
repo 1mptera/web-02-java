5de7b7971d05f6a7f1cb7aa5299a3439dd1d5433
OHJINSEONG <ojseong0828@naver.com>
2022-07-20 17:25:44 +0900

    Diamond-오진성
    완료

--- week02/Diamond/Diamond.java
+++ week02/Diamond/Diamond.java
@@ -12,9 +12,9 @@ public class Diamond {
         System.out.println("도형의 크기를 입력해 주세요(10 미만의 자연수)");
 
         int size = scanner.nextInt();
-        int base = 2 * size - 1;
-        //입력
+        int height = 2 * size - 1;
 
+        //입력
         while (size >= 10) {
             System.out.println("마름모 출력 프로그램");
             System.out.println("------------------");
@@ -25,29 +25,15 @@ public class Diamond {
             System.out.println("다시 입력해 주세요.");
         }
 
-        for (int i = 1; i < size + 1; i += 1) {
-            for (int j = 1; j <= base; j += 1) {
-                if (j >= size - i + 1 && j <= size + i - 1) {
-                    starDiamond += "*";
+        for (int i = 0; i <= height; i += 1) {
+            for (int j = 1; j <= height + 1; j += 1) {
+                if (Math.pow(i, 2) > Math.pow(j - size, 2) && Math.pow(i - 2 * size, 2) > Math.pow(-j + size, 2)) {
+                    System.out.print("*");
                     continue;
                 }
-                starDiamond += " ";
+                System.out.print(" ");
             }
-            starDiamond += "\n";
+            System.out.println();
         }
-        for (int i = 0; i < size; i += 1) {
-            for (int j = 0; j < base; j += 1) {
-                if (j >= i + 1 && j <= base - i - 2) {
-                    starDiamond += "*";
-                    continue;
-                }
-                starDiamond += " ";
-            }
-            starDiamond += "\n";
-        }
-        System.out.println(starDiamond);
-
     }
-
 }
-
