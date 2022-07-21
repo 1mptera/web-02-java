48725ace303dfafa4dc24b5268183e0370888253
OHJINSEONG <ojseong0828@naver.com>
2022-07-21 17:39:12 +0900

    피드백 수정 완료!
    불필요한 변수 제거와 변수 이름바꾸기

--- week02/Square/Square.java
+++ week02/Square/Square.java
@@ -4,24 +4,22 @@ public class Square {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String length = "";
-
         System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
 
-        int answer = scanner.nextInt();
+        int oneSideLength = scanner.nextInt();
 
-        while ((answer < 2) || (answer >= 20)) {
+        while ((oneSideLength < 2) || (oneSideLength >= 20)) {
 
             System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
 
-            answer = scanner.nextInt();
+            oneSideLength = scanner.nextInt();
 
             System.out.println("다시 입력해주세요.");
         }
 
-        if (answer >= 2 && answer < 20) {
-            for (int i = 0; i < answer; i += 1) {
-                for (int j = 0; j < answer; j += 1) {
+        if (oneSideLength >= 2 && oneSideLength < 20) {
+            for (int i = 0; i < oneSideLength; i += 1) {
+                for (int j = 0; j < oneSideLength; j += 1) {
                     System.out.print("*");
                     continue;
                 }
