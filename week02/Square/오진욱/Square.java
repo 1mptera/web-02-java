10d68abd2c198542c32957a16914f3a4416c3fe5
jingwook <qnfdkf153@gmail.com>
2022-07-21 15:43:25 +0900

    입력과 처리의 로직 분리
    while문에 입력 조건을 넣어 입력과 처리의 로직을 분리했다.

--- week02/Square/오진욱/Square.java
+++ week02/Square/오진욱/Square.java
@@ -5,28 +5,27 @@ public class Square {
         //준비
         Scanner scanner = new Scanner(System.in);
 
-        int finish = 0;
+        String star = "";
 
-        while (finish < 1) {
-            //입력
-            System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
-            int squareLength = scanner.nextInt();
+        boolean exit = false;
 
-            if (squareLength < 2 || squareLength >= 20) {
-                System.out.println("다시 입력해주세요");
-                continue;
+        //입력
+        System.out.println("한 변의 길이를 입력해 주세요(2 이상 20 미만)");
+        int squareLength = scanner.nextInt();
+
+        while (squareLength < 2 || squareLength >= 20) {
+            System.out.println("다시 입력해주세요");
+
+            squareLength = scanner.nextInt();
+        }
+
+        //처리
+        for (int i = 0; i < squareLength; i += 1) {
+            for (int j = 0; j < squareLength; j += 1) {
+                System.out.print("*");
             }
-
-            //처리
-            for (int i = 0; i < squareLength; i += 1) {
-                for (int j = 0; j < squareLength; j += 1) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-
             //출력
-            finish = 1;
+            System.out.println();
         }
     }
 }
