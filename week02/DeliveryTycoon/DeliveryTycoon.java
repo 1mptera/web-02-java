6d6b01f69e310b8de522b2f4db1c9b14bb6b6538
OHJINSEONG <ojseong0828@naver.com>
2022-07-21 16:07:27 +0900

    수정

--- week02/DeliveryTycoon/DeliveryTycoon.java
+++ week02/DeliveryTycoon/DeliveryTycoon.java
@@ -5,40 +5,18 @@ public class DeliveryTycoon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
         String[] foodMenu = new String[]{"1. 햄버거", "2. 피자", "3. 치킨", "4. 커피", "5. 케이크", "6. 족발"};
-        int[] price = new int[]{1, 5, 3, 1, 2, 4};
-
-        for (int i = 0; i < 20; i += 1) {
-            int x = random.nextInt(6);
-            int y = random.nextInt(6);
-            String temp = foodMenu[x];
-            foodMenu[x] = foodMenu[y];
-            foodMenu[y] = temp;
-        }
+        int[] foodPrice = new int[]{1, 5, 3, 1, 2, 4};
+        int[] select = new int[]{1, 2, 3, 4, 5, 6};
 
         int date = 0;
         int money = 1000;
         int hp = 1000;
         int popularity = 0;
+        int ordersPrice = 0;
 
         String[] storeName = new String[]{"노점상", "푸드 트럭", "구멍 가게", "레스토랑"};
         String currentLevel = "";
-
-
-        if (money <= 2000 && popularity <= 20) {
-            currentLevel = storeName[0];
-        }
-        if (money <= 5000 && money > 2000 && popularity <= 50 && popularity > 20) {
-            currentLevel = storeName[1];
-        }
-        if (money <= 8000 && money > 5000 && popularity <= 80 && popularity > 500) {
-            currentLevel = storeName[2];
-        }
-        if (money > 8000 && popularity > 80) {
-            currentLevel = storeName[3];
-        }
-
 
         System.out.println("---------------");
         System.out.println("딜리버리 타이쿤 게임");
@@ -57,6 +35,17 @@ public class DeliveryTycoon {
         while (true) {
             date += 1;
 
+            currentLevel = storeName[0];
+            if (money > 2000 && popularity > 20) {
+                currentLevel = storeName[1];
+            }
+            if (money > 5000 && popularity > 50) {
+                currentLevel = storeName[2];
+            }
+            if (money > 8000 && popularity > 80) {
+                currentLevel = storeName[3];
+            }
+
             System.out.println(date + "일차 " + name + "의 " + currentLevel);
             System.out.println("--------------------------------------------");
             System.out.println("체력: " + hp + ", 자산: " + money + "만원, 인지도: " + popularity);
@@ -64,30 +53,68 @@ public class DeliveryTycoon {
             System.out.println("1. 가게 오픈하기");
             System.out.println("2. 상점");
             System.out.println("3. 메뉴로 돌아가기");
-
             choose = scanner.nextInt();
-
 
             if (choose == 1) {
                 for (int i = 0; i < 5; i += 1) {
 
+                    int[] numberOfOrders = new int[]{1, 2, 3, 4, 5, 6};
+
+                    for (int j = 0; j < 20; j += 1) {
+                        int x = random.nextInt(6);
+                        int y = random.nextInt(6);
+                        int temp4 = numberOfOrders[x];
+                        numberOfOrders[x] = numberOfOrders[y];
+                        numberOfOrders[y] = temp4;
+                    }
+
                     System.out.println("--------------------------------------------");
                     System.out.println("체력: " + hp + ", 자산: " + money + "만원, 인지도: " + popularity);
                     System.out.println("--------------------------------------------");
-                    System.out.println("6개의 주문이 들어 왔습니다. (10초 안에 음식을 완성해주세요.)");
+                    System.out.println(numberOfOrders[i] + "개의 주문이 들어 왔습니다. (10초 안에 음식을 완성해주세요.)");
 
-                    int randomFood = random.nextInt(6);
-                    for (int j = 0; j <= randomFood; j += 1) {
-                        System.out.println(foodMenu[j]);
-                        System.out.println("음식준비:");
-                        choose = scanner.nextInt();
-                        if( )
+                    for (int j = 0; j < 20; j += 1) {
+                        int x = random.nextInt(6);
+                        int y = random.nextInt(6);
+                        String temp1 = foodMenu[x];
+                        foodMenu[x] = foodMenu[y];
+                        foodMenu[y] = temp1;
+                        int temp2 = foodPrice[x];
+                        foodPrice[x] = foodPrice[y];
+                        foodPrice[y] = temp2;
+                        int temp3 = select[x];
+                        select[x] = select[y];
+                        select[y] = temp3;
                     }
 
+
+                    for (int j = 0; j < numberOfOrders[i]; j += 1) {
+                        System.out.println(foodMenu[j]);
+                        ordersPrice += foodPrice[j];
+                    }
+
+                    for (int j = 0; j < numberOfOrders[i]; j += 1) {
+                        System.out.println("음식 준비: ");
+                        choose = scanner.nextInt();
+
+                        if (choose != select[j]) {
+                            money -= ordersPrice;
+                            System.out.println("주문이 틀렸습니다! " + ordersPrice + " 만원의 손해가 발생했습니다.");
+                            System.out.println("내 자산: " + money);
+                            System.out.println();
+                            break;
+                        }
+                    }
+                    money += ordersPrice;
+                    System.out.println("음식 준비 완료! 소요시간: ");
+                    System.out.println("배달완료! 수익: " + ordersPrice);
+                    System.out.println();
                 }
             }
-            while (choose != 4) {
-                if (choose == 2) {
+
+
+            if (choose == 2) {
+                while (choose != 4) {
                     System.out.println("-----------------------");
                     System.out.println("플렉스 상점");
                     System.out.println("-----------------------");
@@ -105,6 +132,7 @@ public class DeliveryTycoon {
                         System.out.println("자양강장제 구입! 내 체력: " + hp);
                         continue;
                     }
+
                     if (choose == 2) {
                         popularity += 10;
                         money -= 300;
@@ -112,6 +140,7 @@ public class DeliveryTycoon {
                         System.out.println("인지도 상승! 내 인지도: " + popularity);
                         continue;
                     }
+
                     if (choose == 3) {
                         int gambling = random.nextInt(2);
                         boolean succes = gambling == 1;
@@ -121,16 +150,16 @@ public class DeliveryTycoon {
 
                         if (succes) {
                             money *= 2;
-                            System.out.println("복권 성공! 내 자산: " + money);
+                            System.out.println("도박 성공! 내 자산: " + money);
                         }
                         if (!succes) {
-                            System.out.println("복권 실패! 내 자산: " + money);
+                            System.out.println("도박 실패! 내 자산: " + money);
                         }
+                        date += 1;
                         continue;
                     }
                 }
             }
-
 
             if (choose == 3) {
                 System.out.println("메뉴를 선택해주세요");
@@ -139,6 +168,7 @@ public class DeliveryTycoon {
 
                 choose = scanner.nextInt();
                 if (choose == 1) {
+                    date = 1;
                     System.out.println("주인공 이름을 입력해주세요");
 
                     name = scanner.next();
@@ -150,6 +180,5 @@ public class DeliveryTycoon {
 
             }
         }
-
     }
 }
