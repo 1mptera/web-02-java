5c65951a36005dee0dd45d784d193909685e5c6a
jingwook <qnfdkf153@gmail.com>
2022-07-21 17:39:44 +0900

    Math.abs와 마름모의 수학적 성질을 이용해 간단하게 문제 해결!
    부등식이 여기쓰이네...

--- week02/Diamond/오진욱/Diamond.java
+++ week02/Diamond/오진욱/Diamond.java
@@ -4,49 +4,36 @@ public class Diamond {
     public static void main(String[] args) {
         //준비
         Scanner scanner = new Scanner(System.in);
-        String star = "";
 
         System.out.println("마름모 출력 프로그램");
         System.out.println("-------------------");
+
         System.out.println("도형의 크기를 입력해 주세요(10 미만의 자연수)");
 
-        while (true) {
-            //입력
-            int diamondLength = scanner.nextInt();
-            int diamondHeight = 2 * diamondLength - 1;
+        //입력
+        int diamondLength = scanner.nextInt();
+        int diamondHeight = 2 * diamondLength - 1;
 
-            //처리
-            if (diamondLength < 1 || diamondLength > 10) {
-                System.out.println("다시 입력해주세요");
-                continue;
-            }
+        //처리
+        while (diamondLength < 1 || diamondLength >= 10) {
+            System.out.println("다시 입력해주세요");
 
-            for (int i = 1; i <= diamondHeight; i += 1) {
-                for (int j = 1; j <= diamondHeight; j += 1) {
-                    if (i <= diamondLength) {
-                        if (Math.abs(j - diamondLength) <= Math.abs(i - 1)) {
-                            System.out.print("*");
+            diamondLength = scanner.nextInt();
+        }
 
-                            continue;
-                        }
-                        System.out.print(" ");
-                    }
+        for (int i = 1; i <= diamondHeight; i += 1) {
+            for (int j = 1; j <= diamondHeight; j += 1) {
+                boolean upperHalf = Math.abs(i) > Math.abs(j - diamondLength);
+                boolean lowerHalf = Math.abs(i - diamondHeight) >= Math.abs(j - diamondLength);
 
-                    if (i > diamondLength) {
-                        if (j <= i - diamondLength || j >= diamondHeight - (i - diamondLength) + 1) {
-                            System.out.print(" ");
+                if (upperHalf && lowerHalf) {
+                    System.out.print("*");
 
-                            continue;
-                        }
-                        System.out.print("*");
-                    }
+                    continue;
                 }
-                System.out.println("");
+                System.out.print(" ");
             }
-
-            //출력
-            System.out.println(star);
-            return;
+            System.out.println();
         }
     }
 }
