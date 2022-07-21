302d1305a684838603ae647198915621ec24d9d6
jingwook <qnfdkf153@gmail.com>
2022-07-21 19:35:26 +0900

    foodDB 줄바꿈 수정, 이중배열 수정.
    너무 긴 foodDB배열을 적절하게 줄바꿈 해주었고, 불필요한 이중배열을 삭제했다.

--- week02/LunchWorldcup/오진욱/LunchWorldcup.java
+++ week02/LunchWorldcup/오진욱/LunchWorldcup.java
@@ -7,33 +7,39 @@ public class LunchWorldcup {
 
         int roundCount = 6;
 
-        String[][] round = new String[roundCount - 1][];
+//        for (int i = 1; i <= roundCount; i += 1) {
+//            int length = (int) Math.pow(2, roundCount - i);
+//            String [] round = new String[length];
+//        }
 
-        for (int i = 1; i <= round.length; i += 1) {
-            int length = (int) Math.pow(2, round.length - i);
-            round[i - 1] = new String[length];
-        }
+        String [] temp = new String[(int)Math.pow(2, roundCount -1)];
 
-        String[] foodDB = new String[]{"피자", "햄버거", "돈까스", "카레", "짜장면", "탄탄면", "떡볶이", "파스타", "연어덮밥", "초밥", "라멘", "쌀국수", "곰탕", "국밥", "파타이", "텐동", "제육볶음", "쭈꾸미 볶음", "부대찌개", "김치찌개", "우동", "냉면", "냉모밀", "국수", "생선구이", "육회비빔밥", "물회국수", "뼈해장국", "닭갈비", "마라탕", "샌드위치", "타코"};
+        String[] foodDB = new String[]{"피자", "햄버거", "돈까스", "카레", "짜장면", "탄탄면", "떡볶이", "파스타", "연어덮밥", "초밥",
+                "라멘", "쌀국수", "곰탕", "국밥", "파타이", "텐동", "제육볶음", "쭈꾸미 볶음", "부대찌개", "김치찌개", "우동", "냉면",
+                "냉모밀", "국수", "생선구이", "육회비빔밥", "물회국수", "뼈해장국", "닭갈비", "마라탕", "샌드위치", "타코"};
 
-        for (int i = 0; i < round.length; i += 1) {
+        for (int i = 1; i <roundCount; i += 1) {
+            int length = (int) Math.pow(2, roundCount -i);
 
-            if (i == 0) {
-                for (int j = 0; j < round[i].length; j += 1) {
-                    round[i][j] = foodDB[j];
+            String [] round = new String[length];
 
+            for (int j = 0; j < round.length; j += 1) {
+                if ( i == 1){
+                    round[j] = foodDB[j];
+                    continue;
                 }
-                continue;
+                round[j] = temp[j];
             }
+
 
             System.out.println("");
             System.out.println("-----------------");
 
-            if (i < round.length - 1) {
-                System.out.println("점심 메뉴 월드컵 " + round[i].length + "강");
+            if (i < roundCount -1) {
+                System.out.println("점심 메뉴 월드컵 " + round.length/2 + "강");
             }
 
-            if (i == round.length - 1) {
+            if (i == roundCount -1) {
                 System.out.println("점심 메뉴 월드컵 결승전!");
             }
 
@@ -42,12 +48,12 @@ public class LunchWorldcup {
             System.out.println("선호하는 메뉴를 번호로 선택해주세요");
             System.out.println("");
 
-            for (int j = 0; j < round[i - 1].length; j += 2) {
+            for (int j = 0; j < round.length; j += 2) {
                 int x = j;
-                System.out.println("1. " + round[i - 1][x]);
+                System.out.println("1. " + round[x]);
 
                 x += 1;
-                System.out.println("2. " + round[i - 1][x]);
+                System.out.println("2. " + round[x]);
 
                 int select = scanner.nextInt();
 
@@ -58,16 +64,15 @@ public class LunchWorldcup {
                 }
 
                 if (select == 1) {
-                    round[i][j / 2] = round[i - 1][j];
+                    temp[j / 2] = round[j];
                 }
 
                 if (select == 2) {
-                    round[i][j / 2] = round[i - 1][j + 1];
+                    temp[j / 2] = round[j + 1];
                 }
             }
         }
-
-        String lunch = round[round.length - 1][0];
+        String lunch = temp[0];
 
         System.out.println();
         System.out.println("오늘의 점심은 " + lunch + " 입니다");
