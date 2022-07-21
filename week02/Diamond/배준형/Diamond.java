9423be7934feb08def233ae4f366b96a27767bb1
junhyeong00 <jhbae0420@naver.com>
2022-07-21 23:04:39 +0900

    한 번의 로직으로 작동하도록 수정

--- week02/Diamond/배준형/Diamond.java
+++ week02/Diamond/배준형/Diamond.java
@@ -9,32 +9,22 @@ public class Diamond {
         System.out.println("도형의 크기를 입력해 주세요(10 미만의 자연수)");
 
         int size = scanner.nextInt();
+        int height = 2 * size -1;
 
-        while (0 >= size || size >= 10) {
+        while (size <= 0 || size >= 10) {
             System.out.println("다시 입력해주세요");
 
             size = scanner.nextInt();
         }
-        
-        for (int i = 0; i < size; i += 1) {
-            for (int j = size - 1; j > i; j -= 1) {
+
+        for (int i = 0; i <= height; i += 1) {
+            for (int j = 1; j <= height + 1; j += 1) {
+                if (Math.abs(i) > Math.abs(j - size) && Math.abs(i - 2 * size) > Math.abs(- j + size)){
+                    System.out.print("*");
+                    continue;
+                }
+
                 System.out.print(" ");
-            }
-
-            for (int j = -1; j < i * 2; j += 1) {
-                System.out.print("*");
-            }
-
-            System.out.println();
-        }
-
-        for (int i = 1; i < size; i += 1) {
-            for (int j = 0; j < i; j += 1) {
-                System.out.print(" ");
-            }
-
-            for (int j = size * 2 - 1; j > i * 2; j -= 1) {
-                System.out.print("*");
             }
 
             System.out.println();
