ea680c21c0f7764bc851df99ba8c11656467f8f0
jingwook <qnfdkf153@gmail.com>
2022-07-22 15:28:13 +0900

    과제 완성!

--- week02/BaseballGameForTwo/오진욱/BaseballGameForTwo.java
+++ week02/BaseballGameForTwo/오진욱/BaseballGameForTwo.java
@@ -1,17 +1,35 @@
 //mvp : 각각의 정답을 가지는 숫자야구 만들기 (strike, ball, 턴 넘기기)
 //단 1번의 S, B 처리가 끝나고 출력 후에 2번의 출력이 이루어져야 한다.
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class BaseballGameForTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         int[] strike = new int[2];
         int[] ball = new int[2];
 
-        int[] number1 = new int[]{4, 6, 1};
-        int[] number2 = new int[]{8, 3, 6};
+        int[] number1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] number2 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < 20; i +=1 ){
+            int x1 = random.nextInt(10);
+            int y1 = random.nextInt(10);
+            int temp1 = number1[x1];
+
+            number1[x1] = number1[y1];
+            number1[y1] = temp1;
+
+            int x2 = random.nextInt(10);
+            int y2 = random.nextInt(10);
+            int temp2 = x2;
+
+            number1[x2] = number1[y2];
+            number1[y2] = temp2;
+        }
 
         int[] answer = new int[3];
 
@@ -25,14 +43,12 @@ public class BaseballGameForTwo {
         System.out.println(longDash);
 
         while (strike[0] < 3 && strike[1] < 3) {
-            System.out.println("player: " + player);
+            int index = player - 1;
 
             for (int i = 0; i < strike.length; i += 1) {
                 strike[i] = 0;
                 ball[i] = 0;
             }
-
-            int index = player - 1;
 
             int[] number = new int[3];
 
