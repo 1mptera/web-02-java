5948d88a3b944d50de572e7ba0d46a6e972edae6
jingwook <qnfdkf153@gmail.com>
2022-07-22 10:59:42 +0900

    가독성을 위해 다시 조건문 추가, 연관된 초기값(상태)끼리 순서 배치 변경
    배달 비즈니스 로직의 뎁스가 깊어서 예외처리를 이용해 조건문을 삭제했는데, 가독성이 떨어져 다시 조건문을 기입하였다.

--- week02/DeliveryTycoon/오진욱/DeliveryTycoon.java
+++ week02/DeliveryTycoon/오진욱/DeliveryTycoon.java
@@ -27,11 +27,10 @@ public class DeliveryTycoon {
 
         String[] menus = new String[]{"1. 햄버거", "2. 피자", "3. 치킨", "4. 커피", "5. 케이크", "6. 족발"};
         int[] foodPrices = new int[]{1, 5, 3, 1, 2, 4};
+        int[] orders = new int[]{1, 2, 3, 4, 5, 6};
 
         String[] stores = new String[]{"노점상", "푸드 트럭", "구멍가게", "레스토랑"};
         String level = "";
-
-        int[] orders = new int[]{1, 2, 3, 4, 5, 6};
 
         while (!exit) {
             day = 1;
@@ -39,7 +38,6 @@ public class DeliveryTycoon {
             money = 1000;
             fame = 0;
 
-            playing = false;
 
             //입력
             System.out.println(shortDash);
@@ -62,13 +60,14 @@ public class DeliveryTycoon {
                 exit = true;
             }
 
+            playing = true;
+
             System.out.println("이름을 입력해주세요 : ");
 
             scanner.nextLine();
             name = scanner.nextLine();
 
-            while (!playing) {
-                shop = false;
+            while (playing) {
 
                 if (money <= 2000 && fame <= 20) {
                     level = stores[0];
@@ -96,18 +95,134 @@ public class DeliveryTycoon {
 
                 if (select != 1 && select != 2 && select != 3) {
                     System.out.println("다시 선택해 주세요.");
+
                     continue;
                 }
 
                 if (select == 3) {
-                    playing = true;
+                    playing = false;
+
+                    continue;
+                }
+
+                if (select == 1) {
+
+                    if (hp < 500) {
+                        System.out.println("체력이 부족합니다. 체력을 보충해주세요! 내 체력: " + hp);
+                        System.out.println();
+
+                        continue;
+                    }
+
+                    for (int i = 0; i < 20; i += 1) {
+                        int x = random.nextInt(orders.length);
+                        int y = random.nextInt(orders.length);
+                        int temp = orders[x];
+
+                        orders[x] = orders[y];
+                        orders[y] = temp;
+                    }
+
+                    totalGet = 0;
+
+                    for (int i = 0; i < 5; i += 1) {
+                        for (int j = 0; j <= 20; j += 1) {
+                            int x = random.nextInt(6);
+                            int y = random.nextInt(6);
+
+                            String tempMenu = menus[x];
+                            menus[x] = menus[y];
+                            menus[y] = tempMenu;
+
+                            int tempPrice = foodPrices[x];
+                            foodPrices[x] = foodPrices[y];
+                            foodPrices[y] = tempPrice;
+                        }
+
+                        deliveryFee = 0;
+                        deliveryOk = true;
+
+                        long start = System.currentTimeMillis();
+
+                        int order = orders[i];
+
+                        System.out.println(longDash);
+                        System.out.println("체력 : " + hp + ", 자산 : " + money + "만 원, 인지도: " + fame);
+                        System.out.println(longDash);
+
+                        System.out.println(order + "개의 음식주문이 들어왔습니다. (10초안에 음식을 완성해주세요!)");
+
+                        for (int j = 0; j < order; j += 1) {
+                            System.out.println(menus[j]);
+
+                            deliveryFee += foodPrices[j];
+                        }
+
+                        for (int j = 0; j < order; j += 1) {
+                            int current = Integer.parseInt(menus[j].substring(0, 1));
+
+                            System.out.println("음식준비:");
+
+                            select = scanner.nextInt();
+
+                            if (select != current) {
+                                deliveryOk = false;
+
+                                break;
+                            }
+                        }
+
+                        time = (System.currentTimeMillis() - start) / 1000;
+
+                        if (deliveryOk == false) {
+                            totalGet -= deliveryFee;
+                            money -= deliveryFee;
+
+                            System.out.println("주문이 틀렸습니다! " + deliveryFee + "만원의 손해가 발생했습니다.");
+
+                            System.out.println("내 자산: " + money);
+                        }
+
+                        if (deliveryOk == true) {
+                            System.out.println("음식 준비 완료! 소요시간: " + time);
+
+                            if (time > 10) {
+                                totalGet -= deliveryFee;
+                                money -= deliveryFee;
+                                fame -= 1;
+
+                                System.out.println("주문 시간이 초과했습니다! " + deliveryFee + " 만원의 손해가 발생했습니다.");
+
+                                System.out.println("내 자산: " + money);
+                                continue;
+                            }
+
+                            totalGet += deliveryFee;
+                            money += deliveryFee;
+                            fame += 1;
+
+                            System.out.println("배달완료! 수익: " + deliveryFee + " 만원");
+                        }
+                        System.out.println();
+
+                    }
+                    System.out.println(day + "일차 가게 마감!");
+                    System.out.println("총 수익: " + totalGet + "만 원");
+                    System.out.println("인지도: " + fame);
+                    System.out.println("체력소모: -500");
+                    System.out.println();
+
+                    day += 1;
+                    hp -= 500;
                     continue;
                 }
 
                 if (select == 2) {
+                    shop = true;
+
                     String[] item = new String[]{"자양강장제", "광고", "도박"};
 
-                    while (!shop) {
+                    while (shop) {
                         System.out.println(shortDash);
                         System.out.println("플렉스 상점");
                         System.out.println(shortDash);
@@ -120,11 +235,13 @@ public class DeliveryTycoon {
 
                         if (select != 1 && select != 2 && select != 3 && select != 4) {
                             System.out.println("1, 2, 3, 4번 중에 골라 주세요!");
+
                             continue;
                         }
 
                         if (select == 4) {
-                            shop = true;
+                            shop = false;
+
                             continue;
                         }
 
@@ -134,6 +251,7 @@ public class DeliveryTycoon {
 
                             System.out.println(item[0] + " 구입! 내 자산: " + money);
                             System.out.println("체력 회복! 내 체력: " + hp);
+
                             continue;
                         }
 
@@ -143,132 +261,26 @@ public class DeliveryTycoon {
 
                             System.out.println(item[1] + " 구입! 내 자산: " + money);
                             System.out.println("인지도 상승! 내 인지도: " + fame);
+
                             continue;
                         }
 
-                            money -= 50;
+                        money -= 50;
 
-                            System.out.println(item[2] + " 구입! 내 자산: " + money);
-                            int gamble = random.nextInt(10);
+                        System.out.println(item[2] + " 구입! 내 자산: " + money);
+                        int gamble = random.nextInt(10);
 
-                            if (gamble < 5) {
-                                money *= 2;
+                        if (gamble < 5) {
+                            money *= 2;
 
-                                System.out.println("도박 성공! 내 자산: " + money);
-                                continue;
-                            }
-
-                            System.out.println("도박 실패! 내 자산: " + money);
-                    }
-                    continue;
-                }
-
-                if (hp < 500) {
-                    System.out.println("체력이 부족합니다. 체력을 보충해주세요! 내 체력: " + hp);
-                    System.out.println();
-                    continue;
-                }
-
-                for (int i = 0; i < 20; i += 1) {
-                    int x = random.nextInt(orders.length);
-                    int y = random.nextInt(orders.length);
-                    int temp = orders[x];
-
-                    orders[x] = orders[y];
-                    orders[y] = temp;
-                }
-
-                totalGet = 0;
-
-                for (int i = 0; i < 5; i += 1) {
-                    for (int j = 0; j <= 20; j += 1) {
-                        int x = random.nextInt(6);
-                        int y = random.nextInt(6);
-
-                        String tempMenu = menus[x];
-                        menus[x] = menus[y];
-                        menus[y] = tempMenu;
-
-                        int tempPrice = foodPrices[x];
-                        foodPrices[x] = foodPrices[y];
-                        foodPrices[y] = tempPrice;
-                    }
-
-                    deliveryFee = 0;
-                    deliveryOk = true;
-
-                    long start = System.currentTimeMillis();
-                    int order = orders[i];
-
-                    System.out.println(longDash);
-                    System.out.println("체력 : " + hp + ", 자산 : " + money + "만 원, 인지도: " + fame);
-                    System.out.println(longDash);
-
-                    System.out.println(order + "개의 음식주문이 들어왔습니다. (10초안에 음식을 완성해주세요!)");
-
-                    for (int j = 0; j < order; j += 1) {
-                        System.out.println(menus[j]);
-
-                        deliveryFee += foodPrices[j];
-                    }
-
-                    for (int j = 0; j < order; j += 1) {
-                        int current = Integer.parseInt(menus[j].substring(0, 1));
-
-                        System.out.println("음식준비:");
-
-                        select = scanner.nextInt();
-
-                        if (select != current) {
-                            deliveryOk = false;
-
-                            break;
-                        }
-                    }
-
-                    time = (System.currentTimeMillis() - start) / 1000;
-
-                    if (deliveryOk == false) {
-                        totalGet -= deliveryFee;
-                        money -= deliveryFee;
-
-                        System.out.println("주문이 틀렸습니다! " + deliveryFee + "만원의 손해가 발생했습니다.");
-
-                        System.out.println("내 자산: " + money);
-                    }
-
-                    if (deliveryOk == true) {
-                        System.out.println("음식 준비 완료! 소요시간: " + time);
-
-                        if (time > 10) {
-                            totalGet -= deliveryFee;
-                            money -= deliveryFee;
-                            fame -= 1;
-
-                            System.out.println("주문 시간이 초과했습니다! " + deliveryFee + " 만원의 손해가 발생했습니다.");
-
-                            System.out.println("내 자산: " + money);
+                            System.out.println("도박 성공! 내 자산: " + money);
                             continue;
                         }
 
-                        totalGet += deliveryFee;
-                        money += deliveryFee;
-                        fame += 1;
-
-                        System.out.println("배달완료! 수익: " + deliveryFee + " 만원");
+                        System.out.println("도박 실패! 내 자산: " + money);
                     }
-                    System.out.println();
-
+                    continue;
                 }
-                System.out.println(day + "일차 가게 마감!");
-                System.out.println("총 수익: " + totalGet + "만 원");
-                System.out.println("인지도: " + fame);
-                System.out.println("체력소모: -500");
-                System.out.println();
-
-                day += 1;
-                hp -= 500;
-                continue;
             }
         }
     }
