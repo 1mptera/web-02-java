1ed193779500fc53912831a73e046e8f9f622159
jingwook <qnfdkf153@gmail.com>
2022-07-24 00:56:26 +0900

    불필요한 임시변수 삭제 및 재할당 수정

--- week02/LunchWorldcup/오진욱/LunchWorldcup.java
+++ week02/LunchWorldcup/오진욱/LunchWorldcup.java
@@ -5,35 +5,28 @@ public class LunchWorldcup {
         //준비
         Scanner scanner = new Scanner(System.in);
 
-        int roundCount = 6;
-
-        String [] temp = new String[(int)Math.pow(2, roundCount -1)];
-
         String[] foodDB = new String[]{"피자", "햄버거", "돈까스", "카레", "짜장면", "탄탄면", "떡볶이", "파스타", "연어덮밥", "초밥",
                 "라멘", "쌀국수", "곰탕", "국밥", "파타이", "텐동", "제육볶음", "쭈꾸미 볶음", "부대찌개", "김치찌개", "우동", "냉면",
                 "냉모밀", "국수", "생선구이", "육회비빔밥", "물회국수", "뼈해장국", "닭갈비", "마라탕", "샌드위치", "타코"};
 
-        for (int i = 1; i <roundCount; i += 1) {
-            int length = (int) Math.pow(2, roundCount -i);
+        int roundCount = 5;
 
-            String [] foods = new String[length];
 
-            for (int j = 0; j < foods.length; j += 1) {
-                if ( i == 1){
-                    foods[j] = foodDB[j];
-                    continue;
-                }
-                foods[j] = temp[j];
-            }
+        String[] foods = foodDB;
+
+        for (int i = 1; i <= roundCount; i += 1) {
+            int length = (int) Math.pow(2, roundCount - i);
+
+            String[] nextRound = new String[length];
 
             System.out.println();
             System.out.println("-----------------");
 
-            if (i < roundCount -1) {
-                System.out.println("점심 메뉴 월드컵 " + foods.length/2 + "강");
+            if (i < roundCount ) {
+                System.out.println("점심 메뉴 월드컵 " + foods.length / 2 + "강");
             }
 
-            if (i == roundCount -1) {
+            if (i == roundCount) {
                 System.out.println("점심 메뉴 월드컵 결승전!");
             }
 
@@ -58,15 +51,16 @@ public class LunchWorldcup {
                 }
 
                 if (select == 1) {
-                    temp[j / 2] = foods[j];
+                    nextRound[j / 2] = foods[j];
                 }
 
                 if (select == 2) {
-                    temp[j / 2] = foods[j + 1];
+                    nextRound[j / 2] = foods[j + 1];
                 }
             }
+            foods = nextRound;
         }
-        String lunch = temp[0];
+        String lunch = foods[0];
 
         System.out.println();
         System.out.println("오늘의 점심은 " + lunch + " 입니다");
